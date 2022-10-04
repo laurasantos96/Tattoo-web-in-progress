f802b80a4d91cc6459b1b920aeaea7c0aa78b866
Laura Santos <laurasantosvilas96@gmail.com>
2022-10-04 20:32:15 +0200

    work

---
+++ main.js
@@ -0,0 +1,6 @@
+let login = document.querySelector('#toggleLogin');
+
+login.addEventListener('click', () => (
+    let loginSide = document.querySelector('#loginSide');
+    loginSide.classList.add('active_login');
+))
